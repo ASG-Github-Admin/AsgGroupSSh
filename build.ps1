6aaca6b60b593bda4500a3d6347c75d7d2212fbe
Robert Woodward <Robert.Woodward@asggroup.com.au>
2019-10-22 09:00:43 +1000

    !deploy

--- build.ps1
+++ build.ps1
@@ -5,6 +5,7 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Install-Module -Name Psake, PSDeploy, BuildHelpers, PSScriptAnalyzer -Force
 Install-Module -Name Pester -Force -SkipPublisherCheck
+Install-Module -Name Posh-SSH -Force
 Import-Module -Name Psake, BuildHelpers
 
 Set-BuildEnvironment
